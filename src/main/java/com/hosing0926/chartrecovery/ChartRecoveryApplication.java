cc0fcfc0047fc6c1b7f6ce5d77d03451a0d62fe8
hosing0926 <choimax0926@gmail.com>
2024-04-13 20:54:24 +0900

    Initial Commit

---
+++ src/main/java/com/hosing0926/chartrecovery/ChartRecoveryApplication.java
@@ -0,0 +1,13 @@
+package com.hosing0926.chartrecovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartRecoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChartRecoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hosing0926/chartrecovery/ChartRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosing0926.chartrecovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartRecoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
